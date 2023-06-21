c790c5ee46905347d15615babad0508607c5bbe8
HMXHKAKA <dreamkaka457@gmail.com>
2023-06-21 22:41:48 +0800

    update

---
+++ data.source.js
@@ -0,0 +1,512 @@
+import React from 'react';
+export const Nav20DataSource = {
+  isScrollLink: true,
+  wrapper: { className: 'header2 home-page-wrapper lj5srol0wip-editor_css' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header2-logo',
+    children:
+      'https://f003.backblazeb2.com/file/pic-pan/MW%CE%A4%CE%9B%C3%98%CE%9A%CE%9E-docs.png',
+  },
+  LinkMenu: {
+    className: 'header2-menu',
+    children: [
+      {
+        name: 'linkNav~lj5su025kch',
+        to: 'https://mwtaoke-docs.netlify.app/',
+        children: 'MWΤΛØΚΞ-DOCS',
+        className: 'menu-item lj5tk18q5gi-editor_css',
+      },
+    ],
+  },
+  mobileMenu: { className: 'header2-mobile-menu' },
+};
+export const Banner30DataSource = {
+  wrapper: { className: 'banner3 lj4ehza1h4o-editor_css' },
+  textWrapper: {
+    className: 'banner3-text-wrapper',
+    children: [
+      {
+        name: 'nameEn',
+        className: 'banner3-name-en',
+        children: (
+          <span>
+            <p>
+              <br />
+            </p>
+          </span>
+        ),
+      },
+      {
+        name: 'slogan',
+        className: 'banner3-slogan lj4h1w2nsk-editor_css',
+        children: 'MWΤΛØΚΞ',
+        texty: true,
+      },
+      {
+        name: 'name',
+        className: 'banner3-name lj4eg5z63p-editor_css',
+        children: (
+          <span>
+            <span>
+              <p>一个现代战舰民间非官方项目</p>
+            </span>
+          </span>
+        ),
+      },
+      {
+        name: 'time',
+        className: 'banner3-time lj5svpwe7oj-editor_css',
+        children: (
+          <span>
+            <p>2.0.0beta01 首页</p>
+          </span>
+        ),
+      },
+    ],
+  },
+};
+export const Content130DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content13-wrapper lj4g6ft65dn-editor_css',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>MWΤΛØΚΞ</p>
+          </span>
+        ),
+        className: 'title-h1 lj4g7o7qng-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <p>
+                我们是一个成立不久的现代战舰非官方非盈利性民间组织，皆在为大家提供游戏最新资讯，新手教程和一些自己的看法
+              </p>
+            </span>
+          </span>
+        ),
+        className: 'title-content lj4g6eiwbma-editor_css',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <p>目前主要以教程产出和装备评价为主</p>
+          </span>
+        ),
+        className: 'title-content lj4gbb90nx-editor_css',
+      },
+    ],
+  },
+};
+export const Teams10DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper teams1-wrapper lj4gnmp6qme-editor_css',
+  },
+  page: { className: 'home-page teams1' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: '团队成员',
+        className: 'lj4gnl48l08-editor_css',
+      },
+    ],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://f003.backblazeb2.com/file/pic-pan/1681579785696.jpg',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title lj4go0pbv5o-editor_css',
+              children: (
+                <span>
+                  <p>幻梦晓寒kaka</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>网站开发者</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <span>
+                    <p>负责网站主要开发</p>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children: 'https://f003.backblazeb2.com/file/pic-pan/sky.jpg',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title lj5so7ua4gf-editor_css',
+              children: (
+                <span>
+                  <p>
+                    <span>SKY</span>
+                    <br />
+                  </p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>编辑</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <p>负责市场评价板块编写</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children: 'https://f003.backblazeb2.com/file/pic-pan/feiji.jpg',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title lj5t594kiio-editor_css',
+              children: (
+                <span>
+                  <span>
+                    <p>很忙很忙的李某某</p>
+                  </span>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>编辑</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <span>
+                    <p>负责教程编写</p>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Banner20DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0 lj4gu2okgab-editor_css' },
+        title: {
+          className: 'banner2-title lj4gcdaqw5f-editor_css',
+          children: (
+            <span>
+              <p>MWΤΛØΚΞ-DOCS</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'banner2-content lj4gcw8gnfj-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>一个简单的现代战舰文档</p>
+              </span>
+            </span>
+          ),
+        },
+        button: {
+          className: 'banner2-button lj4ge5jablo-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>开始阅读</p>
+              </span>
+            </span>
+          ),
+          href: 'https://mwtaoke-docs.netlify.app/',
+        },
+      },
+    ],
+  },
+};
+export const Feature00DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper content0-wrapper lj5sg120rhq-editor_css',
+  },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>特性</p>
+            </span>
+          </span>
+        ),
+        className: 'lj5silx5ant-editor_css',
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title lj5sgc1on4s-editor_css',
+              children: (
+                <span>
+                  <p>萌新教程</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>给萌新提供快速提升等级和刷钱的教程</p>
+                </span>
+              ),
+              className: 'lj5sgow1k1q-editor_css',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title lj5sgbbtheh-editor_css',
+              children: (
+                <span>
+                  <span>
+                    <p>市场装备评价</p>
+                  </span>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>对市场装备进行评价，给出我们的建议帮助您判断</p>
+                </span>
+              ),
+              className: 'lj5sgawolpj-editor_css',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title lj5sip6gyef-editor_css',
+              children: (
+                <span>
+                  <p>共同进步</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>自己也想贡献？当然可以！</p>
+                </span>
+              ),
+              className: 'lj5sj53erhq-editor_css',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Banner21DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0 lj5t80zr2q-editor_css' },
+        title: {
+          className: 'banner2-title lj5taqn64s-editor_css',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <p>MWΤΛØΚΞ-MIRROR</p>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+        content: {
+          className: 'banner2-content',
+          children: (
+            <span>
+              <p>
+                现代战舰apk<span>下载站</span>
+              </p>
+            </span>
+          ),
+        },
+        button: {
+          className: 'banner2-button lj5tca7gggn-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>敬请期待</p>
+              </span>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper footer0-wrapper lj4gsk4j78e-editor_css',
+  },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>©2023&nbsp;</span>MWΤΛØΚΞ<span>&nbsp;All Rights Reserved</span>
+      </span>
+    ),
+  },
+};
---
+++ index.jsx
@@ -0,0 +1,124 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
+import React from 'react';
+import { enquireScreen } from 'enquire-js';
+
+import Nav2 from './Nav2';
+import Banner3 from './Banner3';
+import Content13 from './Content13';
+import Teams1 from './Teams1';
+import Banner2 from './Banner2';
+import Feature0 from './Feature0';
+import Footer0 from './Footer0';
+
+import {
+  Nav20DataSource,
+  Banner30DataSource,
+  Content130DataSource,
+  Teams10DataSource,
+  Banner20DataSource,
+  Feature00DataSource,
+  Banner21DataSource,
+  Footer00DataSource,
+} from './data.source';
+import './less/antMotionStyle.less';
+
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+const { location = {} } = typeof window !== 'undefined' ? window : {};
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+      show: !location.port, // 如果不是 dva 2.0 请删除
+    };
+  }
+
+  componentDidMount() {
+    // 适配手机屏幕;
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+    // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
+    /* 如果不是 dva 2.0 请删除 start */
+    if (location.port) {
+      // 样式 build 时间在 200-300ms 之间;
+      setTimeout(() => {
+        this.setState({
+          show: true,
+        });
+      }, 500);
+    }
+    /* 如果不是 dva 2.0 请删除 end */
+  }
+
+  render() {
+    const children = [
+      <Nav2
+        id="Nav2_0"
+        key="Nav2_0"
+        dataSource={Nav20DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner3
+        id="Banner3_0"
+        key="Banner3_0"
+        dataSource={Banner30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content13
+        id="Content13_0"
+        key="Content13_0"
+        dataSource={Content130DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Teams1
+        id="Teams1_0"
+        key="Teams1_0"
+        dataSource={Teams10DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner2
+        id="Banner2_0"
+        key="Banner2_0"
+        dataSource={Banner20DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature0
+        id="Feature0_0"
+        key="Feature0_0"
+        dataSource={Feature00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner2
+        id="Banner2_1"
+        key="Banner2_1"
+        dataSource={Banner21DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer0
+        id="Footer0_0"
+        key="Footer0_0"
+        dataSource={Footer00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+    ];
+    return (
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+        {/* 如果不是 dva 2.0 替换成 {children} start */}
+        {this.state.show && children}
+        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      </div>
+    );
+  }
+}
